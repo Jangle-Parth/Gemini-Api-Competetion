b97a7fa5d002d0e7e0d1ad43c3b3b3c9a9f22e85
Jangle <onlyforparthcoding+github@gmail.com>
2024-08-11 00:47:38 +0530

    initial commit

---
+++ lib/libraryscreen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class LibraryScreen extends StatelessWidget {
+  LibraryScreen({super.key});
+  final List<String> imagePaths = [
+    'assets/image1.jpg',
+    'assets/image2.jpg',
+    'assets/image3.jpg',
+    'assets/image4.jpg',
+    'assets/image5.jpg',
+    'assets/image6.jpg',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color(0xff071739),
+        leading: IconButton(
+          icon: const Icon(
+            Icons.arrow_back,
+            color: Colors.white,
+            size: 30,
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: GridView.builder(
+          itemCount: imagePaths.length,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3,
+            crossAxisSpacing: 10.0,
+            mainAxisSpacing: 10.0,
+          ),
+          itemBuilder: (context, index) {
+            return Container(
+              color: Colors.grey[300],
+              child: Image.asset(
+                imagePaths[index],
+                fit: BoxFit.cover,
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
